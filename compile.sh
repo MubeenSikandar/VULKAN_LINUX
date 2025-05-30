6c37a530e3b5f2b86c6da5b7bba4840063401159
Muhammad Mubeen Sikandar <68024800+MubeenSikandar@users.noreply.github.com>
2025-05-30 17:20:58 +0500

    Delete compile.sh

--- compile.sh
+++
@@ -1,2 +0,0 @@
-/Users/mubeensikandar/VulkanSDK/1.4.313.0/macOS/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-/Users/mubeensikandar/VulkanSDK/1.4.313.0/macOS/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
