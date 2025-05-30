390b31889c65739b1828b317f56975ec23034740
MubeenSikandar <mmubeen.sikandar@gmail.com>
2025-05-30 17:43:26 +0500

    Update .gitignore and stop tracking build files

--- compile.sh
+++
@@ -1,2 +0,0 @@
-/home/mubeen/Downloads/1.4.313.0/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-/home/mubeen/Downloads/1.4.313.0/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
